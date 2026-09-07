341cde94d171630b9ee733f974fa6d489fe262c8
Aditya pratap singh <167612148+adityaps70@users.noreply.github.com>
2026-09-08 01:47:19 +0530

    fix: render shared logo without image optimizer

--- src/components/brand/wordmark.tsx
+++ src/components/brand/wordmark.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
 
 export function Wordmark() {
   return (
@@ -8,14 +8,13 @@ export function Wordmark() {
       aria-label="Sea N Shore home"
       className="inline-flex shrink-0 items-center rounded-lg"
     >
-      <Image
+      <img
         src="/brand/sea-and-shore-logo.webp"
         alt="Sea and Shore Global Shipping Community"
-        width={360}
-        height={320}
-        priority
+        width={180}
+        height={160}
         className="h-[58px] w-auto object-contain sm:h-[64px]"
       />
     </Link>
-  );
+  )
 }
