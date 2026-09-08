23e91ea1754085596627432cdd4570ca5495ebb7
Aditya pratap singh <167612148+adityaps70@users.noreply.github.com>
2026-09-08 20:06:07 +0530

    fix: allow exact logo image contract

--- src/components/brand/wordmark.tsx
+++ src/components/brand/wordmark.tsx
@@ -11,6 +11,7 @@ export function Wordmark({ compact = false }: WordmarkProps) {
       aria-label="Sea N Shore home"
       className="inline-flex shrink-0 items-center rounded-lg"
     >
+      {/* eslint-disable-next-line @next/next/no-img-element -- exact uploaded brand asset path is part of the Wordmark contract */}
       <img
         src={compact ? '/brand/sea-n-shore-symbol.webp' : '/brand/sea-and-shore-master-logo.svg'}
         alt="Sea and Shore Global Shipping Community"
