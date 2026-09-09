96f6240c2cd744fd6b011e4fe441878fd5270444
Aditya pratap singh <167612148+adityaps70@users.noreply.github.com>
2026-09-09 16:29:58 +0530

    feat: add job queries

---
+++ src/features/jobs/queries.ts
@@ -0,0 +1,39 @@
+import { requireAwsUser, type AwsVerifiedUser } from '@/features/auth/aws-queries'
+import { jobsRepository, type JobsRepository } from './repository'
+
+type RequireUser = () => Promise<AwsVerifiedUser>
+
+export function createJobsQueries(input: { requireUser: RequireUser; repository: JobsRepository }) {
+  async function getPublishedJobs() {
+    await input.requireUser()
+    return input.repository.listPublishedJobs(50)
+  }
+
+  async function getPublishedJob(id: string) {
+    await input.requireUser()
+    return input.repository.getPublishedJob(id)
+  }
+
+  async function getMyJobApplications() {
+    const user = await input.requireUser()
+    return input.repository.listApplications(user.id, 50)
+  }
+
+  async function getJobApplicationState(id: string) {
+    const user = await input.requireUser()
+    const [job, alreadyApplied] = await Promise.all([
+      input.repository.getPublishedJob(id),
+      input.repository.hasApplied(id, user.id),
+    ])
+    return { job, alreadyApplied }
+  }
+
+  return { getPublishedJobs, getPublishedJob, getMyJobApplications, getJobApplicationState }
+}
+
+const productionQueries = createJobsQueries({ requireUser: requireAwsUser, repository: jobsRepository })
+
+export const getPublishedJobs = productionQueries.getPublishedJobs
+export const getPublishedJob = productionQueries.getPublishedJob
+export const getMyJobApplications = productionQueries.getMyJobApplications
+export const getJobApplicationState = productionQueries.getJobApplicationState
