15036366d87c7aea0f2e3643465eb4d3f5bb100a
Aditya pratap singh <167612148+adityaps70@users.noreply.github.com>
2026-09-13 15:05:51 +0530

    infra: add realtime terraform providers

---
+++ infra/aws/app/providers-realtime.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
