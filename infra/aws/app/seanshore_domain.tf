d07f5f95c940ee7a8d5cecddb012a30bfdd23009
Aditya pratap singh <167612148+adityaps70@users.noreply.github.com>
2026-09-24 13:38:10 +0530

    feat: preserve legacy seanshore DNS and add ACM validation records

--- infra/aws/app/seanshore_domain.tf
+++ infra/aws/app/seanshore_domain.tf
@@ -25,3 +25,37 @@ resource "aws_acm_certificate" "seanshore_edge" {
     Name = "seanshore.in edge certificate"
   })
 }
+
+
+resource "aws_route53_record" "seanshore_legacy_apex" {
+  zone_id = aws_route53_zone.seanshore.zone_id
+  name    = "seanshore.in"
+  type    = "A"
+  ttl     = 300
+  records = ["162.215.226.7"]
+}
+
+resource "aws_route53_record" "seanshore_legacy_www" {
+  zone_id = aws_route53_zone.seanshore.zone_id
+  name    = "www.seanshore.in"
+  type    = "A"
+  ttl     = 300
+  records = ["162.215.226.7"]
+}
+
+resource "aws_route53_record" "seanshore_edge_validation" {
+  for_each = {
+    for option in aws_acm_certificate.seanshore_edge.domain_validation_options :
+    option.domain_name => {
+      name   = option.resource_record_name
+      type   = option.resource_record_type
+      record = option.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.seanshore.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 300
+  records = [each.value.record]
+}
