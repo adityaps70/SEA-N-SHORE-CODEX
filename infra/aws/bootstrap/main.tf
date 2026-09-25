08e1bf3644e6385047c4114f96061fb233816a7b
Aditya pratap singh <167612148+adityaps70@users.noreply.github.com>
2026-09-25 20:50:25 +0530

    fix: allow read-only Cognito signup capacity telemetry

--- infra/aws/bootstrap/main.tf
+++ infra/aws/bootstrap/main.tf
@@ -303,6 +303,15 @@ resource "aws_iam_role_policy" "github_deploy" {
         Resource = "arn:aws:cognito-idp:${var.aws_region}:${data.aws_caller_identity.current.account_id}:userpool/${var.phase5b_cognito_user_pool_id}"
       },
       {
+        Sid    = "ReviewCognitoSignupCapacity"
+        Effect = "Allow"
+        Action = [
+          "cognito-idp:GetProvisionedLimit",
+          "cloudwatch:GetMetricStatistics"
+        ]
+        Resource = "*"
+      },
+      {
         Sid    = "ManageStagingMediaCors"
         Effect = "Allow"
         Action = [
